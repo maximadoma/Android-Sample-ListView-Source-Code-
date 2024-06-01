ca25de407ec3d4999bec43dd638e2545d8c91323
maximadoma <fernylmaningo@gmail.com>
2024-06-01 10:49:40 +0800

    initial commit

---
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.listviewexample;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private ArrayAdapter aAdapter;
+    private String[] users = { "Suresh Dasari", "Rohini Alavala", "Trishika Dasari", "Praveen Alavala", "Madav Sai", "Hamsika Yemineni"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.userlist);
+        aAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, users);
+        mListView.setAdapter(aAdapter);
+    }
+}
